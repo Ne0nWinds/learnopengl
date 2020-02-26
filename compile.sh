03a6ae28754a03db843f1218fae041f9204613fc
Caden Parker <cadenparker@ymail.com>
2020-02-25 19:00:39 -0800

    Drew a triangle

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-clang main.c -o main.exec -I/usr/include `pkg-config --libs glfw3 glew --cflags cglm`
+clang main.c -o main.exec `pkg-config --libs glfw3 glew`
--- main.c
+++ main.c
@@ -7,9 +7,88 @@
 #include <cglm/cglm.h>
 #include <cglm/call.h>
 
+unsigned int VBO, VAO;
+unsigned int vertexShader, fragmentShader, shaderProgram;
+
+char* readFile(const char path[])
+{
+	unsigned int len = 1;
+	unsigned int index = 0;
+	char* str = (char *)malloc(sizeof(char) * len);
+	char* temp;
+	char c;
+	FILE* file = fopen(path, "r");
+	if (!file)
+		goto error;
+	c = getc(file);
+	while (c != EOF)
+	{
+		str[index] = c; index++;
+		if (index == len) {
+			len *= 2;
+			if (NULL == (temp = (char *)realloc(str, sizeof(char) * len)))
+				goto error;
+			else
+				str = temp;
+		}
+		c = getc(file);
+	}
+	if (NULL == (temp = (char *)realloc(str, sizeof(char) * (index + 1))))
+		goto error;
+	else
+		str = temp;
+	str[index] = '\0';
+	fclose(file);
+
+	return str;
+error:
+	free(str);
+	free(temp);
+	fclose(file);
+	printf("Not enough memory\n");
+	exit(-1);
+}
+
 void resize_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0,0,width,height);
+}
+
+void compileShader(GLuint* shader,GLenum type,const char path[])
+{
+	*shader = glCreateShader(type);
+	const char* shaderSource = readFile(path);
+	glShaderSource(*shader, 1, &shaderSource, NULL);
+	glCompileShader(*shader);
+	free((void *)shaderSource);
+
+	int success = 0;
+	char infoLog[512];
+	glGetShaderiv(*shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(*shader, 512, NULL, infoLog);
+		fprintf(stderr, "Error compiling the %d shader: '%s'\n", type, infoLog);
+		exit(-1);
+	}
+}
+
+void attachShaders()
+{
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	int success = 0;
+	char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		fprintf(stderr, "Error linking to program: '%s'\n", infoLog);
+		exit(-1);
+	}
 }
 
 int main(void)
@@ -23,21 +102,47 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600,"",NULL,NULL);
+	GLFWwindow* window = glfwCreateWindow(640, 480,"",NULL,NULL);
+	glfwMakeContextCurrent(window);
+
+	glViewport(0,0,640,480);
+	glfwSetFramebufferSizeCallback(window, resize_callback);
 
 	glewExperimental = GL_TRUE;
 
-	if (!glewInit())
+	if (glewInit() != GLEW_OK)
 	{
-		printf("Failed to load glew");
+		printf("glew failed to init\n");
+		glfwDestroyWindow(window);
+		glfwTerminate();
 		return -1;
 	}
 
-	glViewport(0,0,800,600);
-	glfwSetFramebufferSizeCallback(window, resize_callback);
+	compileShader(&vertexShader, GL_VERTEX_SHADER, "./shaders/vertex.shader");
+	compileShader(&fragmentShader, GL_FRAGMENT_SHADER, "./shaders/fragment.shader");
+
+	attachShaders();
+
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(0.05f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
